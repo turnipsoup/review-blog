b3ad9eaf2177959b191df0a31aa3b973792309b3
galleywest <galleywest@sdf.org>
2022-02-13 15:07:22 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Pardon my dust
---
+++ py/make_blog_from_reviews.py
@@ -0,0 +1,63 @@
+#! /usr/pkg/bin/python3.10
+# 
+
+import os
+import json
+
+BLOG_PATH = "/www/gm/g/galleywest"
+REVIEWS = f"{BLOG_PATH}/reviews"
+
+def readfile(filename) -> str:
+	with open(filename, "r") as f:	
+		filecontents = f.read().strip()
+		return filecontents
+
+def readreview(reviewfile) -> str:
+	propername = f"{REVIEWS}/{reviewfile}"
+	return readfile(propername)
+
+def get_review_title(review) -> str:
+	return review.split("\n")[0].split("#")[-1].strip()
+
+def get_review_subject(review, subject) -> str:
+	review_lines = review.split("\n")
+	for line in review_lines:
+		if f"- {subject}:" in line:
+			return line.split(f"- {subject}:")[-1].strip()
+
+def get_review_subjects(review) -> list[str]:
+	review_lines = review.split("\n")
+	subjects = []
+	for line in review_lines:
+		if line.startswith("-"):
+			subjline = line.split(":")[0].replace("- ", "")
+			return subjline
+
+
+def create_review_object(review):
+	subjects = ["Genres", "Platform", "Start", "End", "Rating", "Notes"]
+	review_object = {}
+	review_lines = review.split("\n")
+	review_title = get_review_title(review)
+	review_object["Title"] = review_title
+
+	for subject in subjects:
+		review_object[subject] = get_review_subject(review, subject)
+	return review_object
+
+if __name__ == "__main__":
+	blog_contents = os.listdir(REVIEWS)
+	blog_json = {}
+	
+	counter = 0	
+	for rawreview in blog_contents:
+		review = readreview(rawreview)
+		blog_json[counter] = create_review_object(review)
+		counter += 1
+	
+	savefile = f"{BLOG_PATH}/reviews.json" 
+	with open(savefile, "w") as f:
+		f.write(json.dumps(blog_json))
+		f.close()
+	os.chmod(savefile, 0o755)
+
